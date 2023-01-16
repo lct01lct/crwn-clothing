7cd228fe1fecd1fe59fcad4a48ee258545940077
刘纯涛 <1786215856@qq.com>
2023-01-16 23:54:50 +0800

    feat: 嵌套路由

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import Home from './routes/home/home.component';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
+
+const Shop = () => {
+  return <div> I am Shop page</div>;
+};
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the Navigation bar</h1>
+      </div>
+      <Outlet></Outlet>
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home></Home>}></Route>
+      <Route path="/" element={<Navigation></Navigation>}>
+        <Route index element={<Home></Home>}></Route>
+        <Route path="shop" element={<Shop></Shop>}></Route>
+      </Route>
     </Routes>
   );
 };
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,7 +1,13 @@
+import { Outlet } from 'react-router-dom';
 import Directory from '@/components/directory/directory.component';
 
 const Home = () => {
-  return <Directory></Directory>;
+  return (
+    <div className="home-container">
+      <Outlet></Outlet>
+      <Directory></Directory>
+    </div>
+  );
 };
 
 export default Home;
